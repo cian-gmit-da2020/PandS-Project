86a8494f809d7164b68c829d2c171e5d5a5e0bda
Cian <G00387860@gmit.ie>
2020-04-02 20:54:54 +0100

    Changes to analysis using dcit and defined function makehist(), output png files

--- analysis.py
+++ analysis.py
@@ -11,6 +11,24 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import seaborn as sns
 
+def makehist(dict, col_name, title, fileout):
+	fig = plt.figure()
+	gs = gridspec.GridSpec(3, 3, figure=fig)
+	ax = fig.add_subplot(gs[:2, :])
+	ax.hist(dict["Entire Dataset"][col_name], bins=15, edgecolor="black",color="purple", label="Entire Dataset")
+	ax.set_title(title)
+	ax1 = fig.add_subplot(gs[2, 0])
+	ax1.hist(dict["Setosa"][col_name],bins=10, edgecolor="black", color="b", label="Setosa")
+	ax1.set_title("Setosa ")
+	ax2 = fig.add_subplot(gs[2, 1])
+	ax2.hist(dict["Versicolor"][col_name],bins=10, edgecolor="black", color="orange", label="Versicolor")
+	ax2.set_title("Versicolor ")
+	ax3 = fig.add_subplot(gs[2, 2])
+	ax3.hist(dict["Virginica"][col_name],bins=10, edgecolor="black", color="g",label="Virginica")
+	ax3.set_title("Virginica ")
+	fig.tight_layout()
+	fig.savefig(fileout)
+
 df = pd.read_csv("iris.csv")
 
 setosa_df = df[df["species"] == "setosa"]
@@ -22,30 +40,20 @@ setosaSum = str(setosa_df.describe())
 versicolorSum = str(versicolor_df.describe())
 virginicaSum = str(virginica_df.describe())
 
+df_dict = {"Entire Dataset": df, "Setosa": setosa_df, 
+		"Virginica": virginica_df, "Versicolor": versicolor_df}
+
 with open("summary.txt", "w+") as f:
 	f.write("Total data set summary \n"+dfSum+"\n")
 	f.write("\nSetosa Species summary \n"+setosaSum+"\n")
 	f.write("\nVersicolor Species summary \n"+versicolorSum+"\n")
 	f.write("\nVirginica Species summary \n"+virginicaSum+"\n")
 
-#dataFrameList = [df, setosa_df, versicolor_df, virginica_df]
+makehist(df_dict, "petal_length", "Petal Length Distribution", "Petal Length Histogram.png")
+makehist(df_dict, "petal_width", "Petal Width Distribution", "Petal Width Histogram.png")
+makehist(df_dict, "sepal_width", "Sepal Width Distribution", "Sepal Width Histogram.png")
+makehist(df_dict, "sepal_length", "Sepal Length Distribution", "Sepal Length Histogram.png")
 
-for col in df.columns:
-	fig = plt.figure()
-	gs = gridspec.GridSpec(3, 3, figure=fig)
-	ax = fig.add_subplot(gs[:2, :])
-	ax.hist(df[col],bins=10, edgecolor="black",color="purple", label="Total "+col+" distribution")
-	ax1 = fig.add_subplot(gs[2, 0])
-	ax1.hist(setosa_df[col],bins=10, edgecolor="black", color="b", label="Setosa")
-	ax2 = fig.add_subplot(gs[2, 1])
-	ax2.hist(versicolor_df[col],bins=10, edgecolor="black", color="orange", label="Versicolor")
-	ax3 = fig.add_subplot(gs[2, 2])
-	ax3.hist(virginica_df[col],bins=10, edgecolor="black", color="g",label="Virginica")
-	fig.suptitle(str(col)+" Total Distribution")
-	# fig.ylabel("Frequency")
-	# fig.xlabel(col)	
-	fig.savefig(col+".png")
-	#plt.clf()
 
 sns.pairplot(df, hue="species")
 plt.show()
