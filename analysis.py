821c6bf0fb1b85c8ef7838a99232950c2fc03b8e
Cian <G00387860@gmit.ie>
2020-04-10 16:35:02 +0100

    added grouped mean, comments, cleaned up code

--- analysis.py
+++ analysis.py
@@ -6,7 +6,6 @@
 # • outputs a scatter plot of each pair of variables.
 
 # import needed libraries
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
@@ -16,17 +15,17 @@ import seaborn as sns
 # The function takes a number of vars to allow for customisation
 def makehist(dict, col_name, bintup, title, fileout):
 	plt.style.use('seaborn') # using the seaborn style sheet
-	# creating the plot figure fig
+	# Creating the plot figure fig
 	fig = plt.figure()
-	# using the grid spec method to create shape needed 
-	# for the 1 large hist and 3 smaller hists in figure fig
+	# Using the grid spec method to create shape needed 
+	# For the 1 large hist and 3 smaller hists in figure fig
 	gs = gridspec.GridSpec(3, 3, figure=fig)
-	# adding first and largest hist
+	# Adding first and largest hist
 	ax = fig.add_subplot(gs[:2, :])
 	ax.hist(dict["Entire Dataset"][col_name], bins=bintup[0], 
 	edgecolor="black",color="y", label="Entire Dataset")
 	ax.set_title(title)
-	# adding next three smaller species hists
+	# Adding next three smaller species hists
 	ax1 = fig.add_subplot(gs[2, 0])
 	ax1.hist(dict["Setosa"][col_name],bins=bintup[1], 
 	edgecolor="black", color="b", label="Setosa")
@@ -41,37 +40,42 @@ def makehist(dict, col_name, bintup, title, fileout):
 	ax3.hist(dict["Virginica"][col_name],bins=bintup[1], 
 	edgecolor="black", color="r",label="Virginica")
 	ax3.set_title("Virginica")
-	# using the tight_layout method for better spacing of the plots
+	# Using the tight_layout method for better spacing of the plots
 	fig.tight_layout()
 	fig.savefig(fileout) # saving the figures to the file path fileout
 
-# load the iris data csv into a dataframs using pandas
+# Load the iris data csv into a dataframe using pandas
 df = pd.read_csv("iris.csv")
 
-# initialise a dataframe for each species type
+# Initialise a dataframe for each species type
 setosa_df = df[df["species"] == "setosa"]
 versicolor_df = df[df["species"] == "versicolor"]
 virginica_df = df[df["species"] == "virginica"]
 
-# create the a brief summary of each dataframe using the describe method
+# Create the a brief summary of each dataframe using the describe method
 dfSum = str(df.describe())
 setosaSum = str(setosa_df.describe())
 versicolorSum = str(versicolor_df.describe())
 virginicaSum = str(virginica_df.describe())
 
-# write each summary datafram to the file summary.txt
-with open("summary.txt", "w+") as f:
-	f.write("All Species Summary \n"+dfSum+"\n")
+# Calculating means by species
+avg = df.groupby("species").mean()
+
+# Write each summary dataframe to the file summary.txt
+with open("summary.txt", "w+") as f: 
+	f.write("Mean values by Species\n"+str(avg)+"\n")
+	f.write("\nAll Species Summary \n"+dfSum+"\n")
 	f.write("\nSetosa Species Summary \n"+setosaSum+"\n")
 	f.write("\nVersicolor Species Summary \n"+versicolorSum+"\n")
 	f.write("\nVirginica Species Summary \n"+virginicaSum+"\n")
+	
 
-# create a dictionary with a label as key and dataframe as value
+# Create a dictionary with a label as key and dataframe as value
 df_dict = {"Entire Dataset": df, "Setosa": setosa_df, 
 "Virginica": virginica_df, "Versicolor": versicolor_df}
 
-# using the function makehist() create a histogram 
-# for each variable for each dataframe
+# Using the function makehist() create a histogram 
+# For each variable for each dataframe
 makehist(df_dict, "petal_length", (15, 10), 
 "Petal Length Distribution All Species", "Petal Length Histogram.png")
 makehist(df_dict, "petal_width", (15, 10), 
@@ -81,7 +85,7 @@ makehist(df_dict, "sepal_width", (15, 10),
 makehist(df_dict, "sepal_length", (15, 10), 
 "Sepal Length Distribution All Species", "Sepal Length Histogram.png")
 
-# using the seaborn pairplot method to plot 
+# Using the seaborn pairplot method to plot 
 # a scatter plot for each pair of variables in the data
-scat = sns.pairplot(df, hue="species")
+sns.pairplot(df, hue="species")
 plt.savefig("Scatter Pairs.png")
